23e4cb6daa9e6665be353efb5600fe08f4bfad0f
anamikajangid <117848984+anamikajangid@users.noreply.github.com>
2023-06-21 11:42:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+    window.onload = function() {
+      var browserInfo = getBrowserInfo();
+      displayMessage(browserInfo);
+    };
+
+    function getBrowserInfo() {
+      var browserName = navigator.appName;
+      var browserVersion = navigator.appVersion;
+      return {
+        name: browserName,
+        version: browserVersion
+      };
+    }
+
+    function displayMessage(browserInfo) {
+      var message = "You are using " + browserInfo.name + " " + browserInfo.version;
+      document.getElementById("browser-info").innerHTML = message;
+    }
+  
