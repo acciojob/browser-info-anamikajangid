864b0d2c780c01eb5543f0a4eca78c41705f4cd2
anamikajangid <117848984+anamikajangid@users.noreply.github.com>
2023-06-25 14:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,62 +1,22 @@
 //your JS code here. If required.
-   
-        // Function to get the browser name and version
-        function getBrowserInfo() {
-            var browserInfo = "Unknown";
-            
-            // Check if the browser is Chrome
-            if (navigator.userAgent.indexOf("Chrome") != -1) {
-                var version = navigator.userAgent.match(/Chrome\/(\d+)\./)[1];
-                browserInfo = "Google Chrome version " + version;
-            }
-            // Check if the browser is Firefox
-            else if (navigator.userAgent.indexOf("Firefox") != -1) {
-                var version = navigator.userAgent.match(/Firefox\/(\d+)\./)[1];
-                browserInfo = "Mozilla Firefox version " + version;
-            }
-            // Check if the browser is Safari
-            else if (navigator.userAgent.indexOf("Safari") != -1) {
-                var version = navigator.userAgent.match(/Version\/(\d+)\./)[1];
-                browserInfo = "Safari version " + version;
-            }
-            // Check if the browser is Internet Explorer
-            else if (navigator.userAgent.indexOf("MSIE") != -1 || navigator.userAgent.indexOf("Trident/") != -1) {
-                var version = navigator.userAgent.match(/MSIE (\d+)\./)[1];
-                browserInfo = "Internet Explorer version " + version;
-            }
-            // Check if the browser is Edge
-            else if (navigator.userAgent.indexOf("Edge") != -1) {
-                var version = navigator.userAgent.match(/Edge\/(\d+)\./)[1];
-                browserInfo = "Microsoft Edge version " + version;
-            }
 
-            return browserInfo;
-        }
+    window.onload = function() {
+      const browserInfo = getBrowserInfo();
+      displayMessage(browserInfo);
+    };
 
-        // Get the browser information and display it on the page
-        var browserInfo = getBrowserInfo();
-        var browserInfoElement = document.getElementById("browser-info");
-        browserInfoElement.textContent = "You are using " + browserInfo;
-   
+    function getBrowserInfo() {
+      const browserName = navigator.appName;
+      const browserVersion = navigator.appVersion;
+      return {
+        name: browserName,
+        version: browserVersion
+      };
+    }
 
-
-    // window.onload = function() {
-    //   const browserInfo = getBrowserInfo();
-    //   displayMessage(browserInfo);
-    // };
-
-    // function getBrowserInfo() {
-    //   const browserName = navigator.appName;
-    //   const browserVersion = navigator.appVersion;
-    //   return {
-    //     name: browserName,
-    //     version: browserVersion
-    //   };
-    // }
-
-    // function displayMessage(browserInfo) {
+    function displayMessage(browserInfo) {
 		
-    //   const message = "You are using " + browserInfo.name + " " + browserInfo.version;
+      const message = "You are using " + browserInfo.name + " " + browserInfo.version;
       document.getElementById("browser-info").innerHTML = message;
     }
   
